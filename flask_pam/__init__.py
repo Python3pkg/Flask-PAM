68f3a963e873b138f33b508d506bd9977924be9c
Python3pkg <raliclo@gmail.com>
2017-05-21 06:48:39 -0700

    Convert to Python3

--- flask_pam/__init__.py
+++ flask_pam/__init__.py
@@ -1,3 +1,3 @@
-from auth import Auth  # pylint: disable=unused-import
-import token_storage  # pylint: disable=unused-import
-import token  # pylint: disable=unused-import
+from .auth import Auth  # pylint: disable=unused-import
+from . import token_storage  # pylint: disable=unused-import
+from . import token  # pylint: disable=unused-import
